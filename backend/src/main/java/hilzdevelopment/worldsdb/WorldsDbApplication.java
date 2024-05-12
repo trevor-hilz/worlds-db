6ac3966723a21704df8027b820711af14f58eafa
Trevor <trevorhilz@gmail.com>
2024-05-12 15:46:40 -0500

    renamed react app folder and project is ready for development.

---
+++ backend/src/main/java/hilzdevelopment/worldsdb/WorldsDbApplication.java
@@ -0,0 +1,13 @@
+package hilzdevelopment.worldsdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorldsDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorldsDbApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/hilzdevelopment/worldsdb/WorldsDbApplicationTests.java
@@ -0,0 +1,13 @@
+package hilzdevelopment.worldsdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorldsDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
